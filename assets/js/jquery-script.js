4b17f99b0ad8e26e860c7cf75a3d4969d0b9be46
Maruyama Ryo <ryo-maruyama@gr.tsrweb.co.jp>
2024-06-09 13:10:05 +0900

    [Change]ブラーがゆっくりかかるように調整

--- assets/js/jquery-script.js
+++ assets/js/jquery-script.js
@@ -5,22 +5,23 @@ jQuery(document).ready(function ($) {
         var section = $('#Context');
         var sectionPosition = section.offset().top;
         var sectionHeight = section.outerHeight();
-
-        // セクションが画面内に表示されているかどうかを確認
+    
         if (scrollPosition + windowHeight / 2 >= sectionPosition && scrollPosition + windowHeight / 2 <= sectionPosition + sectionHeight) {
             $('.video-background video').addClass('blur-background');
         } else {
             $('.video-background video').removeClass('blur-background');
         }
-
+    
         var nextSection = section.next();
         var nextSectionPosition = nextSection.offset().top;
-        var distanceFromTopToHideVideo = 500;
+        var distanceFromTopToHideVideo = 600;
         if (scrollPosition >= nextSectionPosition - distanceFromTopToHideVideo) {
-            $('.video-background video').addClass('hide'); // hideクラスを追加してゆっくり非表示にする
+            $('.video-background video').css('transition', 'opacity 0.5s').css('opacity', '0'); // CSSのトランジションを使用してゆっくり非表示にする
         } else {
-            $('.video-background video').removeClass('hide'); // hideクラスを削除して表示する
+            $('.video-background video').css('transition', 'opacity 0.5s').css('opacity', '1');
         }
     });
-
+    
 });
+
+
