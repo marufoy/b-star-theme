22bffbe62f3159635f6320e226a01a7692719859
Maruyama Ryo <ryo-maruyama@gr.tsrweb.co.jp>
2024-06-18 19:34:41 +0900

    [Add]コンタクトフォームを追加！

--- index.php
+++ index.php
@@ -460,6 +460,24 @@
             </div>
         </section>
 
+        <section id="contact">
+            <div class="section-name">
+                <div class="title-text">
+                    <p class="en-title">CONTACT</p>
+                    <p class="jp-title">お問い合わせ</p>
+                </div>
+                <div class="title-decoration">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+
+            <div>
+                <?php echo do_shortcode('[mwform_formkey key="10"]'); ?>
+            </div>
+        </section>
+
 
     </div>
 
