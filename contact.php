46055768114628ece845b9560572789b8007ca6f
Maruyama Ryo <ryo-maruyama@gr.tsrweb.co.jp>
2024-06-19 15:04:25 +0900

    [Change]トップページとコンタクトページを分離

--- contact.php
+++ contact.php
@@ -7,7 +7,23 @@ Template Name: contact
 <?php get_header(); ?>
 
 <main role="main">
-<?php echo do_shortcode('[mwform_formkey key="10"]'); ?>
+    <section id="contact">
+        <div class="section-name">
+            <div class="title-text">
+                <p class="en-title">CONTACT</p>
+                <p class="jp-title">お問い合わせ</p>
+            </div>
+            <div class="title-decoration">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+
+        <div>
+            <?php echo do_shortcode('[mwform_formkey key="10"]'); ?>
+        </div>
+    </section>
 </main>
 
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -160,27 +160,6 @@ Template Name: top
 
     </div>
 
-    <section id="contact">
-        <div class="section-name">
-            <div class="title-text">
-                <p class="en-title">CONTACT</p>
-                <p class="jp-title">お問い合わせ</p>
-            </div>
-            <div class="title-decoration">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-        </div>
-
-        <div>
-            <?php echo do_shortcode('[mwform_formkey key="10"]'); ?>
-        </div>
-    </section>
-
-
-
-
 </main>
 
 <?php get_footer(); ?>
