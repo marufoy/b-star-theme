c2e4fccf6a87ee3240ca35040276ffba97a728cf
Maruyama Ryo <ryo-maruyama@gr.tsrweb.co.jp>
2024-06-22 11:36:55 +0900

    [add]footer.phpを追加

---
+++ footer.php
@@ -0,0 +1,15 @@
+<?php
+//====================================================
+//  Template Name: Footer
+//====================================================
+?>
+
+<footer>
+    <p>this is footer</p>
+    <span id="copy">&copy; 2022 <?php bloginfo('name'); ?></span>
+</footer>
+</div><!--//class="wrap"-->
+<?php wp_footer(); ?>
+</body>
+
+</html>
