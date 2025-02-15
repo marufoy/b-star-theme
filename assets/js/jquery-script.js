67a4131674da46da9249fb60364f0b6406a49419
ryomaruyama <marusin618@gmail.com>
2025-02-15 10:22:18 +0900

    [Change]slick.jsを分離

--- assets/js/jquery-script.js
+++ assets/js/jquery-script.js
@@ -63,26 +63,6 @@ tabify('#tabs');
 //     ],
 // });
 
-// Initialize additional slick slider
-$('#js-slider').slick({
-    arrows: true, // 前・次のボタンを表示する
-    dots: true, // ドットナビゲーションを表示する
-    // appendDots: $('.dots'), // ドットナビゲーションの生成位置を変更
-    speed: 1000, // スライドさせるスピード（ミリ秒）
-    slidesToShow: 1, // 表示させるスライド数
-    centerMode: true, // slidesToShowが奇数のとき、現在のスライドを中央に表示する
-    variableWidth: true, // スライド幅の自動計算を無効化
-});
-
-$('#js-slider-sp').slick({
-    arrows: false, // 前・次のボタンを表示する
-    dots: true, // ドットナビゲーションを表示する
-    // appendDots: $('.dots'), // ドットナビゲーションの生成位置を変更
-    speed: 1000, // スライドさせるスピード（ミリ秒）
-    slidesToShow: 1, // 表示させるスライド数
-    centerMode: true, // slidesToShowが奇数のとき、現在のスライドを中央に表示する
-    variableWidth: true, // スライド幅の自動計算を無効化
-});
 
 // ScrollReveal animations
 ScrollReveal().reveal('#vision-statement', {
--- header.php
+++ header.php
@@ -9,6 +9,23 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <?php wp_head(); ?>
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/fabicon.png">
+
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-X2WJL4PV2Q"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-X2WJL4PV2Q');
+    </script>
+
+
+
 </head>
 
 <body <?php body_class(); ?>>
